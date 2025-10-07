64a2dce44f9e719a138bfeba0b16b2ff2caa7dec
Rodrigo <rodleidenschaft@gmail.com>
2025-10-07 02:27:33 -0300

    Teste com assertEquals com valor corrigido

--- src/test/java/com/example/taskmanager/service/TaskServiceTest.java
+++ src/test/java/com/example/taskmanager/service/TaskServiceTest.java
@@ -37,7 +37,7 @@ public class TaskServiceTest {
 
         assertNotNull(resultado);
         assertEquals(1L, resultado.getId());
-        assertEquals(" ", resultado.getTitle());
+        assertEquals("Estudar Spring Boot", resultado.getTitle());
         verify(taskRepository, times(1)).save(novaTask);
     }
 
