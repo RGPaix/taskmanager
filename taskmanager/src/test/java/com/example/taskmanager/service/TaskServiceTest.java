adbb4a9747431633dd0d116b51a7b3b1fec581bb
Rodrigo <rodleidenschaft@gmail.com>
2025-10-07 01:13:18 -0300

    Teste com assertEquals com outro valor

--- taskmanager/src/test/java/com/example/taskmanager/service/TaskServiceTest.java
+++ taskmanager/src/test/java/com/example/taskmanager/service/TaskServiceTest.java
@@ -37,7 +37,7 @@ public class TaskServiceTest {
 
         assertNotNull(resultado);
         assertEquals(1L, resultado.getId());
-        assertEquals(" ", resultado.getTitle());
+        assertEquals("Estudar POO", resultado.getTitle());
         verify(taskRepository, times(1)).save(novaTask);
     }
 
